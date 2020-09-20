3807f5262d3b4efe9278ea72261d8c9e13a04f70
Brandon Robinson <brandonscottrobinson@gmail.com>
2020-09-19 21:50:16 -0400

    positiveAs

--- src/app/bored/bored.component.ts
+++ src/app/bored/bored.component.ts
@@ -7,11 +7,27 @@ import { BoredService } from '../bored.service';
 })
 export class BoredComponent implements OnInit {
   activity: any;
+  affirmation: string;
+  affirmations = [
+    'You are safe.',
+    'Anxiety cannot harm you.',
+    'You are brave.',
+    "Everything you've ever wanted is beyond fear.",
+    'Push through the fear!',
+    "Don't let anxiety steal your life.",
+    'You will make it through this difficult moment.',
+    "Give yourself credit for the things you've done.",
+    "Become friends with your anxiety and it can't hurt you anymore.",
+    'Give yourself a break!',
+    'Take a deep breath.',
+  ];
 
   constructor(private service: BoredService) {}
 
   ngOnInit(): void {
     this.getActivities();
+    this.randomAffirmation(this.affirmations);
+    console.log(this.affirmation);
   }
 
   getActivities = () => {
@@ -19,5 +35,9 @@ export class BoredComponent implements OnInit {
       this.activity = response;
       console.log(this.activity);
     });
+  };
+
+  randomAffirmation = (array) => {
+    this.affirmation = array[Math.floor(Math.random() * array.length)];
   };
 }
