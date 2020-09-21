75be07c3dae154bdece80d876f33625ae15159fb
Brandon Robinson <brandonscottrobinson@gmail.com>
2020-09-21 16:44:46 -0400

    fixed iphone 5 screen issue

--- src/app/music.service.ts
+++ src/app/music.service.ts
@@ -9,6 +9,7 @@ export class MusicService {
   brandonKey: string = 'AIzaSyBAU64ophkz8C4KY1z15G-YjFav_DqnRp8';
   troyKey: string = 'AIzaSyCariktb8hHfDYmmGcFoBsGec3mx9YsThM';
   alexKey: string = 'AIzaSyDuMN9t9H0qix9MKhaKt_woH_BSNQUV7y0';
+  newKey: string = 'AIzaSyCBKaKWLOym4eNmN5GwPEjCKkni8k6KwqM';
   auxKey: string = 'AIzaSyCrvNcuOodQMqHWCrQ5AGCXZGerp2yGiLg';
 
   constructor(private http: HttpClient) {}
@@ -16,7 +17,7 @@ export class MusicService {
   getMusic = (subject: string): any => {
     return this.http.get(this.baseUrlMusic, {
       params: {
-        key: this.troyKey,
+        key: this.newKey,
         q: subject,
         part: 'snippet',
         maxResults: '15',
