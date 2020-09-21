7a714de4f1ea13ba55435934a703cca9a1f8f807
Alex <a2weber94@gmail.com>
2020-09-21 16:34:34 -0400

    switched out keys

--- src/app/music.service.ts
+++ src/app/music.service.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MusicService {
   baseUrlMusic: string = 'https://www.googleapis.com/youtube/v3/search';
+  newKey: string = 'AIzaSyCBKaKWLOym4eNmN5GwPEjCKkni8k6KwqM';
   brandonKey: string = 'AIzaSyBAU64ophkz8C4KY1z15G-YjFav_DqnRp8';
   troyKey: string = 'AIzaSyCariktb8hHfDYmmGcFoBsGec3mx9YsThM';
   alexKey: string = 'AIzaSyDuMN9t9H0qix9MKhaKt_woH_BSNQUV7y0';
@@ -16,7 +17,7 @@ export class MusicService {
   getMusic = (subject: string): any => {
     return this.http.get(this.baseUrlMusic, {
       params: {
-        key: this.troyKey,
+        key: this.newKey,
         q: subject,
         part: 'snippet',
         maxResults: '15',
